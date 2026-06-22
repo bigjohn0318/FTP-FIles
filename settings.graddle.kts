8517ef1a66ac469535be15193b9904e7a346da6b
Bigjohn <107620333+bigjohn0318@users.noreply.github.com>
2026-06-22 22:13:59 +0530

    Create settings.graddle.kts

---
+++ settings.graddle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { setUrl("https://repo1.maven.org/maven2") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://repo1.maven.org/maven2") }
+        maven { setUrl("https://artifactory-external.vkpartner.ru/artifactory/maven") }
+        maven { setUrl("https://www.jitpack.io") }
+        mavenLocal()
+    }
+}
+
+rootProject.name = "FlastFTP"
+include(":app")
